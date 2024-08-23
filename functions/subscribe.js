71306480c654a72e2312d50dcfaf200ec26f562a
luis <25castro25@gmail.com>
2024-08-23 05:18:51 -0700

    JSON fix

--- functions/subscribe.js
+++ functions/subscribe.js
@@ -1,11 +1,26 @@
-// functions/subscribe.js
-
 const fs = require("fs");
 const path = require("path");
 
 exports.handler = async (event) => {
   if (event.httpMethod === "POST") {
-    const { email } = JSON.parse(event.body);
+    if (!event.body) {
+      return {
+        statusCode: 400,
+        body: "No data provided",
+      };
+    }
+
+    let email;
+    try {
+      const parsedBody = JSON.parse(event.body);
+      email = parsedBody.email;
+    } catch (error) {
+      return {
+        statusCode: 400,
+        body: "Invalid JSON input",
+      };
+    }
+
     const data = `Email: ${email}\n`;
     const filePath = path.join(__dirname, "../subscribers.txt");
 
