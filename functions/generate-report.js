07cade293375bcc76ced86a953335e14f60b49db
Luis <25castro25@gmail.com>
2025-05-13 16:33:20 -0700

    Email report generatios for EQ Quiz

--- functions/generate-report.js
+++ functions/generate-report.js
@@ -11,7 +11,8 @@ exports.handler = async (event, context) => {
   if (allowedOrigins.includes(origin)) {
     corsHeaders = {
       'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Credentials': 'true',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Requested-With',
       'Access-Control-Allow-Methods': 'POST, OPTIONS',
     };
   }
